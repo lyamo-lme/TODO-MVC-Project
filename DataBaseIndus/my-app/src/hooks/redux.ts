54c693932b87ec6e6f73a62a5feeae04f7ca677f
Maks Limont <ki212_lme@student.ztu.ua>
2022-06-14 19:34:46 +0300

    delete react redux demo named my-app

--- DataBaseIndus/my-app/src/hooks/redux.ts
+++
@@ -1,6 +0,0 @@
-import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
-import {AppDispath, RootState} from "../store/store";
-
-
-export  const useAppDispath =()=>useDispatch<AppDispath>();
-export const useAppSelector:TypedUseSelectorHook<RootState>=useSelector;
--- DataBaseIndus/my-app/src/types/ToDoType.ts
+++
@@ -1,8 +0,0 @@
-export interface IToDoType{
-    nameTask: string,
-    categoryId: number,
-    taskCompleted: boolean,
-    nameCategory: string,
-    id: number,
-    deadLine: Date|null
-}
